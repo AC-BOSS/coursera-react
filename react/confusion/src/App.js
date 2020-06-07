1c6850b9fbe4d8c3582385fe00d2d5547dd4dbf8
AC-BOSS <amartya.dps@gmail.com>
2020-06-08 00:48:20 +0530

    Components Part1

---
+++ react/confusion/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import './App.css';
+import Menu from './components/MenuComponents';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu></Menu>
+      </div>
+    );
+  }
+}
+
+export default App;
