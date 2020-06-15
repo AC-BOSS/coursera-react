8e4d1a4ecc0d6347debff6f40224a35494c6ba95
AC-BOSS <amartya.dps@gmail.com>
2020-06-15 17:35:55 +0530

    Single Page Application-2

--- react/confusion/src/components/DishDetailComponent.js
+++ react/confusion/src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle,BreadcrumbItem, Breadcrumb } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 
     function RenderDish({dish}) {
@@ -40,16 +41,27 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         );
     }
     const DishDetail = (props) => {
-        return(
+        return (
             <div className="container">
-                <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                        {/* {this.renderDish(this.props.dish)} */}
-                        <RenderDish dish={props.dish}/>
-                    </div>
-                    <RenderComments dish={props.dish}/>
-                    {/* {this.renderComments(this.props.dish)} */}
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
                 </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
             </div>
         );
     }
